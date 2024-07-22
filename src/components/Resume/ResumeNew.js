445287bbdf9f31321632851fd644729be47c76ef
SUHAIB C <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-07-22 15:28:58 +0530

    resume download add

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,3 +1,48 @@
+// import React, { useState, useEffect } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import Particle from "../Particle";
+// import pdf from "../../Assets/../Assets/Suhaib's Resume.pdf";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// function ResumeNew() {
+//   const [width, setWidth] = useState(1200);
+
+//   useEffect(() => {
+//     setWidth(window.innerWidth);
+//   }, []);
+
+//   return (
+//     <div>
+//       <Container fluid className="resume-section">
+//         <Particle />
+        
+//         <Row className="resume">
+//           <Document file={pdf} className="d-flex justify-content-center">
+//             <Page pageNumber={1} scale={width > 786 ? 1.3 : 0.4} />
+//           </Document>
+//         </Row>
+
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default ResumeNew;
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -30,7 +75,7 @@ function ResumeNew() {
           <Button
             variant="primary"
             href={pdf}
-            target="_blank"
+            download="Suhaib's Resume.pdf"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
@@ -43,3 +88,5 @@ function ResumeNew() {
 }
 
 export default ResumeNew;
+
+
