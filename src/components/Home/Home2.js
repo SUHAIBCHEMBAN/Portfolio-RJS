13c1c685a19364c9ccb01ee4a380dc424adbb8a2
shuhaib <shuhaibchemban2004@gmail.com>
2024-07-23 12:46:02 +0530

    change profile

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/me.jpeg";
+import myImg from "../../Assets/my.jpeg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
